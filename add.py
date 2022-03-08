7e33887866625822637664bcb2cd67ddafbeab36
tpravin02 <57698051+tpravin02@users.noreply.github.com>
2022-03-08 13:06:40 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+a=4
+b=565
+c=a+b
+print(c)
